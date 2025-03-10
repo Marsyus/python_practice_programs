22e24c0d4801df92c59263ed5a674bf34c149281
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 19:05:07 +0800

    Iterating using while loop

--- odd_numbers.py
+++ odd_numbers.py
@@ -1,3 +1,6 @@
 #Batch02Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 #Iterate from 0 to 100 using while lool
+odd = 0
+while odd <= 100:
+    odd += 1
 #Print the odd numbers
