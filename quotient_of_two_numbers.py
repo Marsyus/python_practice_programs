ac884cabe78a54fcc683efd97a81d121c0fb9102
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:21:04 +0800

    Input two numbers

--- quotient_of_two_numbers.py
+++ quotient_of_two_numbers.py
@@ -1,3 +1,5 @@
 #Batch01Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 #Input two numbers
+num_01 = int(input("Enter first number: "))
+num_02 = int(input("Enter second number: "))
 #Print the quotient of the numbers
