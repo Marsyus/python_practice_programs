260bf7211cbd55aae0105b08250e7a090714d495
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 19:02:56 +0800

    Count the even numbers

--- even_count.py
+++ even_count.py
@@ -5,4 +5,8 @@ for i in range(1, 11):
     number = int(input(f"Enter for number {i}: "))
     numbers.append(number)
 #Count the even numbers
+count = 0
+for even in numbers:
+    if even % 2 == 0:
+        count += 1
 #Print the count
