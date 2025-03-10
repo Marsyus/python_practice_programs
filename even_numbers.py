6672a921e9af5e313852560b45de28a14328fe87
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:39:51 +0800

    Print even numbers from 0 to 100

--- even_numbers.py
+++ even_numbers.py
@@ -1,3 +1,6 @@
 #Batch01Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 #Iterate from 0 to 100
-#Print the even numbers
+for i in range(101):
+    #Print the even numbers
+    if i % 2 == 0:
+        print(i)
