d4fabd4d0563e70ba30e02939ed4ecf040d893b5
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 20:17:01 +0800

    Input ten numbers

--- non_duplicates.py
+++ non_duplicates.py
@@ -1,4 +1,8 @@
 #Batch03Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 #Input ten numbers
+numbers = []
+for i in range(1, 11):
+    number = int(input(f"Enter for number {i}: "))
+    numbers.append(number)
 #Determine which number has a duplicate
 #Print numbers without duplicates
