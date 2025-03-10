5ed60925c54e87b15f01ffb13aebab81054e919f
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 19:01:56 +0800

    Input ten numbers

--- even_count.py
+++ even_count.py
@@ -1,4 +1,8 @@
 #Batch02Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 #Input ten numbers
+numbers = []
+for i in range(1, 11):
+    number = int(input(f"Enter for number {i}: "))
+    numbers.append(number)
 #Count the even numbers
 #Print the count
