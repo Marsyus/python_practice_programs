1ab9f8ec5e5afedb90e7ed5127975ec9f6f8dbce
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 20:07:57 +0800

    Combined the for loops into one and removed redundant lines of code

--- even_count.py
+++ even_count.py
@@ -1,13 +1,10 @@
 #Batch02Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 #Input ten numbers
-numbers = []
+count = 0
 for i in range(1, 11):
     number = int(input(f"Enter for number {i}: "))
-    numbers.append(number)
-#Count the even numbers
-count = 0
-for even in numbers:
-    if even % 2 == 0:
+    #Count the even numbers
+    if number % 2 == 0:
         count += 1
 #Print the count
 print(f"There are {count} even numbers")
