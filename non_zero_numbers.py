4b79cd74322dd4dd6f8affb7d0fb9e8ec2741974
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:41:05 +0800

    Print numbers not ending in zero

--- non_zero_numbers.py
+++ non_zero_numbers.py
@@ -1,3 +1,6 @@
 #Batch01Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 #Iterate from 0 to 100
-#Print the numbers not ending in zero
+for i in range(101):
+    #Print the numbers not ending in zero
+    if i % 10:
+        print(i)
