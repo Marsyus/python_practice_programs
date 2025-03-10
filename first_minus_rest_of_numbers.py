8ce5ea63d0be8584f49133f0cabf9011d581023f
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:55:32 +0800

    Input ten numbers

--- first_minus_rest_of_numbers.py
+++ first_minus_rest_of_numbers.py
@@ -1,4 +1,8 @@
 #Batch02Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 #Input ten numbers
+numbers = []
+for i in range(1, 11):
+    number = int(input(f"Enter for number {i}: "))
+    numbers.append(number)
 #Compute the first number minus the rest
 #Print the result
