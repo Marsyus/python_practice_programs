7aef36adb7f03aa510134156a7d33c82d4e92e20
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 20:48:34 +0800

    Input numbers until input is invalid

--- unique_or_duplicate.py
+++ unique_or_duplicate.py
@@ -1,3 +1,12 @@
 #Batch03Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 #Input a number until the input is invalid
+numbers = []
+count = 1
+while True:
+    try:
+        number = int(input(f"Enter for number {count}: "))
+        count += 1
+        numbers.append(number)
+    except ValueError:
+        break
 #Print "Unique" if the number is not a duplicate. Otherwise, print "Duplicate"
