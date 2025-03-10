262a0203ec56de80baf77ca04189e3878acf9e73
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:48:32 +0800

    Print the smaller number

--- smaller_number.py
+++ smaller_number.py
@@ -3,3 +3,7 @@
 num_01 = int(input("Enter first number: "))
 num_02 = int(input("Enter second number: "))
 #Print the smaller number
+if num_01 < num_02:
+    print(f"{num_01} is smaller")
+elif num_01 > num_02:
+    print(f"{num_02} is smaller")
