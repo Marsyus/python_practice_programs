67b31d196da0bd1d5625d557b569a5784e89a968
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:53:59 +0800

    Input two numbers

--- remainder.py
+++ remainder.py
@@ -1,3 +1,5 @@
 #Batch02Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 #Input two numbers
+num_01 = int(input("Enter first number: "))
+num_02 = int(input("Enter second number: "))
 #Print the remainder of the numbers when divided
