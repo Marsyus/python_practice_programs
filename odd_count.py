9812b568b690a621992c46d174c7ed253ef4d3c6
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 20:09:45 +0800

    Combined the for loops into one and removed redundant lines of code

--- odd_count.py
+++ odd_count.py
@@ -1,13 +1,10 @@
 #Batch01Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 #Input ten numbers
-numbers = []
+count = 0
 for i in range(1, 11):
     number = int(input(f"Enter for number {i}: "))
-    numbers.append(number)
-#Count the odd numbers
-count = 0
-for odd in numbers:
-    if odd % 2:
+    #Count the odd numbers
+    if number % 2:
         count += 1
 #Print the count
 print(f"There are {count} odd numbers")
