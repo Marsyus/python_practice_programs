999327c537d176cd507fb762b4dea2797b8c2c95
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 20:55:31 +0800

    Input numbers until input is invalid

--- lowest_to_highest.py
+++ lowest_to_highest.py
@@ -1,3 +1,12 @@
 #Batch03Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 #Input a number until the input is invalid
+count = 1
+numbers = []
+while True:
+    try:
+        number = int(input(f"Enter for number {count}: "))
+        numbers.append(number)
+        count += 1
+    except ValueError:
+        break
 #Arrange the numbers to print from lowest to highest
