b081810e3fcfbb5799d7b125e12f245314168b8c
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:22:40 +0800

    Input two numbers

--- power_of_two_numbers.py
+++ power_of_two_numbers.py
@@ -1,3 +1,5 @@
 #Batch01Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 #Input two numbers
+num_01 = int(input("Enter first number: "))
+num_02 = int(input("Enter second number: "))
 #Print the power of the numbers
