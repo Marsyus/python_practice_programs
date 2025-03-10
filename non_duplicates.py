302977d103007e16fdb463366d9a4263f616eb1a
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 20:18:07 +0800

    Determine which number has a duplicate

--- non_duplicates.py
+++ non_duplicates.py
@@ -5,4 +5,10 @@ for i in range(1, 11):
     number = int(input(f"Enter for number {i}: "))
     numbers.append(number)
 #Determine which number has a duplicate
+dupes = set()
+seen = set()
+for each in numbers:
+    if each in seen:
+        dupes.add(each)
+    seen.add(each)
 #Print numbers without duplicates
