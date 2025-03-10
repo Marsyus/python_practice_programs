b7e764bc3c4b3fd040ca9ee219f3666d4a125376
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 19:07:07 +0800

    Fixed minor spelling issue

--- odd_numbers.py
+++ odd_numbers.py
@@ -1,5 +1,5 @@
 #Batch02Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
-#Iterate from 0 to 100 using while lool
+#Iterate from 0 to 100 using while loop
 odd = 0
 while odd <= 100:
     odd += 1
