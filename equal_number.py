22a0b9ae464f68261b3b2baf75260d5b89724a00
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:14:49 +0800

    Input two numbers

--- equal_number.py
+++ equal_number.py
@@ -1,3 +1,5 @@
 #Batch01Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 #Input two numbers
+num_01 = int(input("Enter first number: "))
+num_02 = int(input("Enter second number: "))
 #Print "Equal" if the numbers are equal
