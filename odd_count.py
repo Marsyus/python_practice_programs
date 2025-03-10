e3df95e7b0db997b650a332095475d66e45cb4a4
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:35:24 +0800

    Input ten numbers

--- odd_count.py
+++ odd_count.py
@@ -1,4 +1,8 @@
 #Batch01Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 #Input ten numbers
+numbers = []
+for i in range(1, 11):
+    number = int(input(f"Enter for number {i}: "))
+    numbers.append(number)
 #Count the odd numbers
 #Print the count
