628addad5c3047608f8a749189ac6bb3f1cf8156
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 20:53:12 +0800

    Input numbers until input is invalid

--- lowest_number.py
+++ lowest_number.py
@@ -1,3 +1,12 @@
 #Batch03Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 #Input a number until the input is invalid
+numbers = []
+count = 1
+while True:
+    try:
+        number = int(input(f"Enter for number {count}: "))
+        numbers.append(number)
+        count += 1
+    except ValueError:
+        break
 #Arrange the numbers to find and print the lowest
