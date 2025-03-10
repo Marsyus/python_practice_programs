378f5078777349b192d105f560461ad99f7d0d45
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 20:22:15 +0800

    Input ten numbers

--- first_of_duplicates_only.py
+++ first_of_duplicates_only.py
@@ -1,4 +1,8 @@
 #Batch03Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 #Input ten numbers
+numbers = []
+for i in range(1, 11):
+    number = int(input(f"Enter for number {i}: "))
+    numbers.append(number)
 #Determine which number has a duplicate
 #Print all numbers without duplicating
