5ef23a5e99baf14669fc23cd43f729a6505ff098
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 19:09:28 +0800

    Input two numbers

--- numbers_in_between.py
+++ numbers_in_between.py
@@ -1,3 +1,5 @@
 #Batch02Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 #Input two numbers
+num_01 = int(input("Enter first number: "))
+num_02 = int(input("Enter second number: "))
 #Print numbers between the two
