112a76ebb4543fa979c1f938159529c9628ea989
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:11:27 +0800

    Input two numbers

--- bigger_number.py
+++ bigger_number.py
@@ -1,3 +1,5 @@
 #Batch01Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 #Input two numbers
+num_01 = int(input("Enter first number: "))
+num_02 = int(input("Enter second number: "))
 #Print the bigger number
