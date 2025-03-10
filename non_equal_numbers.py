c51cfb21e59774bfb45abcfd6e402ccdc7d950a8
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 18:49:41 +0800

    Input two numbers

--- non_equal_numbers.py
+++ non_equal_numbers.py
@@ -1,3 +1,5 @@
 #Batch02Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 #Input two numbers
+num_01 = int(input("Enter first number: "))
+num_02 = int(input("Enter second number: "))
 #Print "Not Equal" if the numbers are not equal
