6db35c9b7cf735da6c005d6c9f673e5745e27ba7
Marsyus <miadolomboy0917@gmail.com>
2025-03-10 19:13:40 +0800

    Removed redundant lines of code

--- first_minus_rest_of_numbers.py
+++ first_minus_rest_of_numbers.py
@@ -1,10 +1,8 @@
 #Batch02Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 #Input ten numbers
 first_num = 0
-numbers = []
 for i in range(1, 11):
     number = int(input(f"Enter for number {i}: "))
-    numbers.append(number)
     #Compute the first number minus the rest
     if i == 1:
         first_num = number
