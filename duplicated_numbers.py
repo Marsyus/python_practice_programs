629d148e01729c23d4804e882c1355729c9b600a
Marsyus <miadolomboy0917@gmail.com>
2025-03-12 09:41:05 +0800

    Input ten numbers

--- duplicated_numbers.py
+++ duplicated_numbers.py
@@ -1,4 +1,8 @@
 #Batch04Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 #Input ten numbers
+numbers = []
+for i in range(1, 11):
+    num = int(input(f"Enter for number {i}: "))
+    numbers.append(num)
 #Determine which number has a duplicate
 #Print all numbers with duplicates
