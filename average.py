87a73c3d47291a304ddb8319b8bfc5a5d1a83e6b
Marsyus <miadolomboy0917@gmail.com>
2025-03-12 14:03:06 +0800

    Input number until input is invalid

--- average.py
+++ average.py
@@ -1,4 +1,13 @@
 #Batch04Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 #Input a number until the input is invalid
+numbers = []
+count = 1
+while True:
+    try:
+	number = int(input(f"Enter for number {i}: "))
+	numbers.append(number)
+	count += 1
+    except ValueError:
+	break
 #Compute for the average of all numbers
 #Print the result
