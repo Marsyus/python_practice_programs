da69713b7d0c1bf3eaca75e7f285bd2f61144299
Marsyus <miadolomboy0917@gmail.com>
2025-03-12 13:56:03 +0800

    Input number until input is invalid

--- most_duplicated_number.py
+++ most_duplicated_number.py
@@ -1,4 +1,13 @@
 #Batch04Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 #Input a number until the input is invalid
+numbers = []
+count = 1
+while True:
+    try:
+	number = int(input(f"Enter for number {i}: "))
+        numbers.append(number)
+        count += 1
+    except ValueError:
+	break
 #Determine which number has the most duplicates
 #Print the number
